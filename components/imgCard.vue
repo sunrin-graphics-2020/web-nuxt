f48dbf86b355ec3e171cf869eee44ec2dd2376e1
권신학 <ytype33@gmail.com>
2020-07-31 19:06:38 +0900

    ENH: imgCard

--- components/imgCard.vue
+++ components/imgCard.vue
@@ -4,6 +4,9 @@
 
     <div class="middle">
       <div class="text">
+        <p class="author">
+          {{ author }}
+        </p>
         {{ description }}
       </div>
     </div>
@@ -12,7 +15,7 @@
 
 <script>
 export default {
-  props: ['id', 'description'],
+  props: ['id', 'author', 'description'],
   data () {
     return {
       imgSrc: `/2020/work/${this.id}/${this.id}.png`
@@ -25,7 +28,10 @@ export default {
 $backgroundColor: "#4CAF50";
 .card {
   height: 300px;
-
+    .author {
+      display: block;
+      margin-bottom: 5px;
+    }
     img {
       overflow: hidden;
     }
@@ -41,20 +47,21 @@ $backgroundColor: "#4CAF50";
 .middle {
   transition: .5s ease;
   opacity: 0;
+  background-color: black;
   position: absolute;
-  top: 20%;
-  left: 10%;
-  width: 80%;
-  height: 50%;
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  padding: 12px 30px;
   text-align: center;
+  align-items: center;
+  display: flex;
 }
 
 .text {
   display: inline-block; white-space: normal; overflow: hidden; text-overflow: ellipsis;
-
-text-align: left; word-wrap: break-word; display: -webkit-box; -webkit-line-clamp: 6; -webkit-box-orient: vertical;
-
-  background-color: black;
+  text-align: left; word-wrap: break-word; display: -webkit-box; -webkit-line-clamp: 6; -webkit-box-orient: vertical;
   color: white;
   font-size: 14px;
 
--- pages/dynamic/index.vue
+++ pages/dynamic/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="columns is-multiline">
-    <imgCard v-for="(data,idx) in jsonData" :id="1" :key="idx" :description="data['작품설명']" />
+    <imgCard v-for="(data,idx) in jsonData" :id="1" :key="idx" :author="data['이름(영문이름)']" :description="data['작품설명']" />
   </div>
 </template>
 
 <script>
 import helper from '~/assets/js/localStorageHelper'
-// eslint-disable-next-line no-unused-vars
 import imgCard from '@/components/imgCard.vue'
 export default {
   components: { imgCard },
